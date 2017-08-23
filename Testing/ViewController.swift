f7105eea69548925904f0b26cb3c587525591588
bassemanwer <bassem.anwer@apptcom.com>
2017-08-23 18:08:16 +0200

    Initial Commit

---
+++ Testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by Bassem Anwer on 8/23/17.
+//  Copyright © 2017 Bassem Anwer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
